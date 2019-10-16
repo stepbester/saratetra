78f823f5b51cb8e76f27df6231de38f99c3faec5
Stephan <step.bester@gmail.com>
2019-10-16 22:47:38 +0200

    Add unit tests for tetrominoes.

---
+++ js/tests/saratetra.tetrominoes.test.js
@@ -0,0 +1,36 @@
+const Tetrominoes = require("../saratetra.tetrominoes.js");
+
+test("rotatable tetromino 'I' has 4 orientations", () => {
+    var i = new Tetrominoes.I();
+    expect(i.orientation).toBe(0);
+    i.rotate();
+    expect(i.orientation).toBe(1);
+    i.rotate();
+    expect(i.orientation).toBe(2);
+    i.rotate();
+    expect(i.orientation).toBe(3);
+    i.rotate();
+    expect(i.orientation).toBe(0);
+});
+
+test("unrotatable tetromino 'O' has only 1 orientation", () => {
+    var o = new Tetrominoes.O();
+    expect(o.orientation).toBe(0);
+    o.rotate();
+    expect(o.orientation).toBe(0);
+});
+
+test("peek returns the next piece that will be popped", () => {
+    var generator = new Tetrominoes.Generator();
+    var peeked = generator.peek();
+    var popped = generator.pop();
+    expect(peeked).toBe(popped);
+});
+
+test("pop changes the next piece that will be peeked", () => {
+    var generator = new Tetrominoes.Generator();
+    var peek1 = generator.peek();
+    generator.pop();
+    var peek2 = generator.peek();
+    expect(peek1).not.toBe(peek2);
+});
